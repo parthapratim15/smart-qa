b0e1663c786647a9a0d1ce07599db1cbbf026b00
Partha Pratim Sarker <parthacse04@gmail.com>
2024-03-16 01:27:00 +0600

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.main')
+
+@section('title', 'Welcome Page!')
+@push('styles')
+    <style>
+        h1 {
+            margin-top: 20px;
+            text-align: center;
+        }
+        </style>
+@endpush
+@section('content')
+    <h1>Welcome to Section content!</h1>
+@endsection
